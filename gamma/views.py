7dc7f810d352bc89a5175f95a01f89ac33906ae0
Nelson Briceño Bañados <nbriceno@jumpitt.com>
2018-09-15 16:14:31 -0300

    antes de ir a calama

--- gamma/views.py
+++ gamma/views.py
@@ -1,14 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from usuarios.models import Tran
+import requests
+import json
 
 def home(request):
     template_name = 'index.html'
     if 'status' in request.session:
         if request.session['status']=='success':
-            trans = Tran.objects.all()
-            contex = {'trans':trans}
-            return render(request, template_name, contex)
+            r = requests.get('http://192.168.2.133:8050/api/transactions')
+            trans = json.loads(r.text)
+
         else:
             return HttpResponse('''
             <h1>Tu no estas autorizado</h1> <br/>
